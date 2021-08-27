0064e66d34c9dea3875c09d929732b19b04bf7cf
masafumi <masa455.wv@gmail.com>
2021-08-28 00:10:12 +0900

    HeaderTop.jsxをコメント入れて綺麗にまとめた

--- src/components/header/HeaderTop.js
+++ src/components/header/HeaderTop.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { PostMenu } from './PostMenu';
-import Container from '@material-ui/core/Container';
 import walletcss from "./CssMain.module.css";
-
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -11,6 +9,9 @@ import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 
+// material-uiの記述
+// 以下のリンクを参照する
+// https://material-ui.com/components/tabs/
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -23,29 +24,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// 以下のリンクを参照する
-// https://material-ui.com/components/tabs/
-
-
-
-
-
-
-
-
 export const HeaderTop = () => {
+  // material-uiの記述
   const classes = useStyles();
 
-  // ボタン切り替えテスト用
-  const [log, setLog] = useState(true);
-  const switchLog = () => {
-    setLog(!log);
-  };
-
-  const style = {
-    color: '#7c9cdb',
-    paddingRight: '10px',
-  }
+  // ----------ウォレット接続メニューの表示切り替え----------
   const [walletMenue, setWalletMenue] = useState(false);
 
   function ContactWalletOpenmenue() {
@@ -55,47 +38,53 @@ export const HeaderTop = () => {
   function closeMenue() {
     setWalletMenue(false);
   }
+  // ------------------------------------------------------------
+  // -------------ボタン切り替えテスト用(本番時削除)-----------
+  const [log, setLog] = useState(true);
+  const switchLog = () => {
+    setLog(!log);
+  };
 
+  // --ボタンのスタイル本番では消す--
+  const style = {
+    color: '#7c9cdb',
+    paddingRight: '10px',
+  }
+  // --------------------------------------------------------------
   return (
-
-    
-
     <div className={classes.root}>
-
       {/* -----ウォレット接続メニュー（hidden）---- */}
-      {walletMenue && 
-      <>
-      <div onClick={closeMenue} className={walletcss.mask}></div>
-      <div className={walletcss.ContactWalletContainer}>
-        <div className={walletcss.space80}></div>
-        <h1>ウォレットを接続します</h1>
-        <div className={walletcss.space40}></div>
-        <p className={walletcss.sub}>ウォレットを接続することにより、利用規約と<br />
+      {walletMenue &&
+        <>
+          <div onClick={closeMenue} className={walletcss.mask}></div>
+          <div className={walletcss.ContactWalletContainer}>
+            <div className={walletcss.space80}></div>
+            <h1>ウォレットを接続します</h1>
+            <div className={walletcss.space40}></div>
+            <p className={walletcss.sub}>ウォレットを接続することにより、利用規約と<br />
 プライバシーポリシーに同意したことになります。</p>
-        <div className={walletcss.space40}></div>
-        <p className={walletcss.subinfo}>ウォレットを選択してください</p>
-        <div className={walletcss.space20}></div>
-        <div className={walletcss.walletbox}><p>Meta Mask</p></div>
-        <div className={walletcss.space40}></div>
-        <p className={walletcss.sub}>ウォレットとの接続でお困りですか？</p>
-        <div className={walletcss.space10}></div>
-        <p>ウォレット接続に関するQ&A</p>
-        <div className={walletcss.space80}></div>
-      </div>
-      
-      </>
-
-}
+            <div className={walletcss.space40}></div>
+            <p className={walletcss.subinfo}>ウォレットを選択してください</p>
+            <div className={walletcss.space20}></div>
+            <div className={walletcss.walletbox}><p>Meta Mask</p></div>
+            <div className={walletcss.space40}></div>
+            <p className={walletcss.sub}>ウォレットとの接続でお困りですか？</p>
+            <div className={walletcss.space10}></div>
+            <p>ウォレット接続に関するQ&A</p>
+            <div className={walletcss.space80}></div>
+          </div>
+        </>
+      }
       {/* --------------------------- */}
-
       <AppBar position="static">
         <Toolbar>
+          {/* ----企画名---- */}
           <Typography variant="h6" className={classes.title}>
             Anime Post
           </Typography>
+          {/* --------------- */}
           <button onClick={switchLog} style={style}>切り替え</button>
           <BrowserRouter>
-
             {
               // 本番ではログインができていれば
               log
@@ -112,17 +101,9 @@ export const HeaderTop = () => {
                 </Button></Link>
                 </>
             }
-
-            
-              
- 
-
-          
-
           </BrowserRouter>
         </Toolbar>
       </AppBar>
     </div>
-
   );
 };

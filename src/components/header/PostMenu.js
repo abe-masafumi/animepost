319a50c3fb9cfa5e6af7b2c8f04092fbfdc0da36
masafumi <masa455.wv@gmail.com>
2021-08-28 13:33:06 +0900

    PostMenueをコメントつけて見やすくした

--- src/components/header/PostMenu.js
+++ src/components/header/PostMenu.js
@@ -1,7 +1,7 @@
 import { Button } from "@chakra-ui/button"
 import { EditIcon } from "@chakra-ui/icons"
 
-
+// 本番環境で必要かわからない（投稿ボタンをどこに表示するか決まっていない）
 export const PostMenu = () => {
   return (
     <Button leftIcon={<EditIcon />} colorScheme="teal" variant="solid">

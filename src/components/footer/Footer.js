f59f61232de0bf13bda6d2552bacf7dfb6bb17a1
masafumi <masa455.wv@gmail.com>
2021-08-28 14:10:26 +0900

    Footer.jsでfooterのスペースを作成

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,9 @@
+import footercss from "./CssFooter.module.css";
+
 export const Footer = () => {
   return (
-    <>
+    <div className={footercss.FooterContainer}>
     <p>footer</p>
-    </>
+    </div>
   );
 } ;
